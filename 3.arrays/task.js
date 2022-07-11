8c36019eec5a0b56a426ebc6e32e6330fac79823
Виталий Матвиюк <vit-matviyuk@mail.ru>
2022-07-11 17:39:40 +0300

    third commit variables 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  
+  let result = (arr1.length === arr2.length) && (arr1.every((n, i) => n === arr2[i]));
+  
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+function advancedFilter(arr) {
+  
+  let resultArr = arr
+    .filter(currentValue => currentValue > 0 && currentValue % 3 === 0)
+    .map((arr) => arr * 10);  
 
   return resultArr; // array
 }
