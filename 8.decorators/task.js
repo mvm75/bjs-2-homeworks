74e71c3bed900313186c70a81099aa214fe63e4f
Виталий Матвиюк <vit-matviyuk@mail.ru>
2022-08-19 22:35:19 +0300

    eighth commit variables 8.decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,51 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
-
-
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
-}
+  let cache = [];
+  
+  function wrapper(...args) {
+      const hash = args.join(","); 
+      let objectInCache = cache.findIndex((item) => item.hash === hash);
+      if (objectInCache !== -1) { 
+          console.log("Из кэша: " + cache[objectInCache].value);
+          return "Из кэша: " + cache[objectInCache].value;
+      }  
+      let value = func(...args); 
+      cache.push({hash: hash, value: value});
+      if (cache.length >= 5) { 
+        cache.shift();
+  } 
+      console.log("Вычисляем: " + value);
+      return "Вычисляем: " + value;  
+  
+    }
+    return wrapper;
+  }
+  
+  
+  function debounceDecoratorNew(func, ms) {
+    let flag  = false;
+    let timerId  = null;
+  
+    function wrapper(...args) {
+        if (!flag ) {
+            flag  = true;
+            func(...args);
+        }
+        clearTimeout(timerId );
+        timerId  = setTimeout(() => {
+            flag = false;
+            func(...args);
+        }, ms);
+    }
+    return wrapper;
+  }
+  
+  function debounceDecorator2(func, ms) {
+    wrapper.count = 0;
+    let debounceDecorator = debounceDecoratorNew(func, ms);
+    
+    function wrapper(...args) {
+        wrapper.count ++;
+        debounceDecorator(...args);
+    }
+    return wrapper;
+  }
